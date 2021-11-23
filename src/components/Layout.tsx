30ab5b0349e949c5d21d3cef85f8d7afbb4b6d76
Were42 <50260518+Were42@users.noreply.github.com>
2021-11-23 13:45:58 +0100

    submit of layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,1 +1,43 @@
-export {};
+import React, { FC } from 'react';
+
+import {
+  Header, Box, ButtonInvisible, Link
+} from '@primer/components';
+import logo from '../assets/logo.png';
+
+const Layout: FC = ({ children }) => (
+  <>
+    <Header>
+      <Header.Item>
+        <Link href="/">
+          <img src={logo} height="30" alt="logo" />
+        </Link>
+      </Header.Item>
+      <Box sx={{ flexGrow: 1 }} />
+      <Header.Item>
+        <Link href="/games">
+          <ButtonInvisible>Logo</ButtonInvisible>
+        </Link>
+      </Header.Item>
+      <Header.Item>
+        <ButtonInvisible>Login</ButtonInvisible>
+      </Header.Item>
+    </Header>
+    <Box
+      maxWidth="sm"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        pt: 8,
+        gap: 2
+      }}
+    >
+      {children}
+    </Box>
+
+  </>
+);
+export default Layout;
