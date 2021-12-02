40f3b1ca01685a9504ca360ea3ec0f52bdffc9cb
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2021-12-02 23:09:31 +0100

    using language switch in layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,43 +1,52 @@
 import React, { FC } from 'react';
 
 import {
-  Header, Box
+  Header, Box, Dropdown
 } from '@primer/components';
 import Logo from '../assets/logo.png';
+import { useTranslation } from '../hooks/useTranslation';
+import LanguageSwitch from './LanguageSwitch';
 
-const Layout: FC = ({ children }) => (
-  <>
-    <Header padding="4px">
-      <Header.Item>
-        <Header.Link href="/" lineHeight="0">
-          <img src={Logo} height="54" alt="logo" />
-        </Header.Link>
-      </Header.Item>
-      <Box sx={{ flexGrow: 1 }} />
-      <Header.Item>
-        <Header.Link href="/games">
-          List of Games
-        </Header.Link>
-      </Header.Item>
-      <Header.Item>
-        Login
-      </Header.Item>
-    </Header>
-    <Box
-      maxWidth="sm"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        pt: 8,
-        gap: 2
-      }}
-    >
-      {children}
-    </Box>
-
-  </>
-);
+const Layout: FC = ({ children }) => {
+  const trans = useTranslation();
+  return (
+    <>
+      <Header padding="4px">
+        <Header.Item>
+          <Header.Link href="/" lineHeight="0">
+            <img src={Logo} height="54" alt="logo" />
+          </Header.Link>
+        </Header.Item>
+        <Box sx={{ flexGrow: 1 }} />
+        <Header.Item>
+          <Header.Link href="/games">
+            {trans('Games')}
+          </Header.Link>
+        </Header.Item>
+        <Header.Item>
+          {trans('Login')}
+        </Header.Item>
+        <Header.Item>
+          <Dropdown>
+            <LanguageSwitch />
+          </Dropdown>
+        </Header.Item>
+      </Header>
+      <Box
+        maxWidth="sm"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          pt: 8,
+          gap: 2
+        }}
+      >
+        {children}
+      </Box>
+    </>
+  );
+};
 export default Layout;
