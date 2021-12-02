bc718bd1b2fbd5441dc20fd1762899f83a5adc14
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2021-12-02 23:09:18 +0100

    creating language switch

---
+++ src/components/LanguageSwitch.tsx
@@ -0,0 +1,39 @@
+import { Dropdown } from '@primer/components';
+import React, { useEffect } from 'react';
+
+import Flag from 'react-flagkit';
+
+import { useLanguage } from '../hooks/useTranslation';
+
+const LanguageSwitch = () => {
+  const [selectedLang, setLanguage] = useLanguage();
+  useEffect(() => {
+
+  }, [selectedLang]);
+  return (
+    <>
+      <Dropdown.Button>{selectedLang === 'cs' ? <Flag country="CZ" /> : <Flag country="GB" />}</Dropdown.Button>
+      <Dropdown.Menu direction="sw" sx={{ width: 55 }}>
+        <Dropdown.Item>
+          <Flag
+            country="CZ"
+            role="button"
+            onClick={() => {
+              setLanguage('cs');
+            }}
+          />
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <Flag
+            country="GB"
+            role="button"
+            onClick={() => {
+              setLanguage('en');
+            }}
+          />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </>
+  );
+};
+export default LanguageSwitch;
