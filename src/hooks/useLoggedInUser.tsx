c56d3a6e176fa733a6664389591751e07384002c
Were42 <50260518+Were42@users.noreply.github.com>
2021-12-02 22:30:11 +0100

    adding hook for logged user

---
+++ src/hooks/useLoggedInUser.tsx
@@ -0,0 +1,24 @@
+import React, {
+  createContext, Dispatch, FC, SetStateAction, useContext, useState
+} from 'react';
+
+type User = {
+  jwt: string,
+  role: string,
+  lang: string
+}
+type UserState = [User, Dispatch<SetStateAction<User>>];
+
+const UserContext = createContext<UserState>(undefined as never);
+
+export const UserProvider: FC = ({ children }) => {
+  const userState = useState<User>({ jwt: '', role: '', lang: '' });
+  return (
+    <UserContext.Provider value={userState}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useLoggedInUser = () => useContext(UserContext);
+export default useLoggedInUser;
