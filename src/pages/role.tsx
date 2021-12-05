af2cd75bda259455f9e08eb8eff0f767f6dc9f33
Were42 <50260518+Were42@users.noreply.github.com>
2021-12-05 16:17:19 +0100

    first try of role-route

---
+++ src/pages/role.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { Text } from '@primer/components';
+import axios from 'axios';
+import useLoggedInUser from '../hooks/useLoggedInUser';
+import routeTo from '../utils/routeTo';
+import handleErrors from '../utils/handleErrors';
+
+const Role = () => {
+  const [user, setUserIn] = useLoggedInUser();
+
+  const submit = () => {
+    console.log('LALA');
+    axios.post(routeTo('/api/user/me/role'), {}, { headers: { Authorization: user.jwt } })
+      .then((response) => {
+        setUserIn({
+          jwt: user.jwt,
+          role: response.data.role
+        });
+        console.log(response.data.role);
+        localStorage.setItem('role', response.data.role);
+        // TODO: set langContext to response.data.lang
+      })
+      .catch((error) => {
+        console.log('CATCH');
+        handleErrors(error);
+      });
+  };
+  useEffect(() => {
+    submit();
+  }, []);
+  return (
+    <Text>
+      {user.jwt}
+    </Text>
+  );
+};
+export default Role;
