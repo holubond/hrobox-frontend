2365728c98a9a649829460985b69dd0c7a4db92f
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2021-12-05 22:45:11 +0100

    typo in en.ts

--- src/localization/en.ts
+++ src/localization/en.ts
@@ -1,7 +1,7 @@
 export default {
   Games: 'List of games',
   Login: 'Login',
-  ErrEmail: 'This field must contain valid email adress',
+  ErrEmail: 'This field must contain valid email address',
   Submit: 'Submit',
   ErrPassword: 'The password must be at least 6 characters long',
   Log: 'Login',
