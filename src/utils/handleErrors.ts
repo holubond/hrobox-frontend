8ea7fcae3fdf2cd816d31cc2f836356f69abbe8f
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2021-12-06 22:23:30 +0100

    added 409 error to handleError

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -13,6 +13,14 @@ const handle404 = (message: string) => {
   alert(message);
 };
 
+const handle409 = (message: string) => {
+  if (message === 'nameEn') {
+    alert('Tag with that english name already exists.');
+  } else {
+    alert('Tag with that czech name already exists.');
+  }
+};
+
 const handleErrors = (error: any) => {
   switch (error.response.status) {
     case 403:
@@ -20,6 +28,9 @@ const handleErrors = (error: any) => {
       break;
     case 404:
       handle404(error.response.data.message);
+      break;
+    case 409:
+      handle409(error.response.data.message);
       break;
     case 500:
       handle500(error.response.data.message);
