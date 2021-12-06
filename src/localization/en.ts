f9d67742de36b2f245a0d6f7d3e5afc7605f95f5
Ondra Holub <ondrejholub202@gmail.com>
2021-12-06 01:09:18 +0100

    Fix - add commas to avoid syntax error

--- src/localization/en.ts
+++ src/localization/en.ts
@@ -4,7 +4,7 @@ export default {
   ForgPassword: 'Forgot password?',
   Pswdreneval: 'Password renewal',
   ErrEmail: 'This field must contain valid email adress',
-  Submit: 'Submit'
+  Submit: 'Submit',
   'We are sorry but your account was banned!': 'We are sorry, but your account has been blocked!',
   'We are waiting for admins to verif your account.': 'We are waiting for the administrators to verify your account.',
   Refresh: 'Refresh'
