bb83a0f76ca4b75a68a30592d0c01c9bfb7e73b7
Were42 <50260518+Were42@users.noreply.github.com>
2021-12-07 14:07:14 +0100

    adding new routes to switch

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import Role from '../pages/role';
 import Home from '../pages/Home';
 import Games from '../pages/Games';
+import Registration from '../pages/Registration';
+import Forgot from '../pages/Forgot';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/role" exact component={Role} />
     <Route path="/games" exact component={Games} />
+    <Route path="/registration" exact component={Registration} />
+    <Route path="/forgot" exact component={Forgot} />
   </Switch>
 );
 export default Routes;
