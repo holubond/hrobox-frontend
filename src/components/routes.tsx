171a804caa17113198d3baac2a479a2b63fe1ca7
Were42 <50260518+Were42@users.noreply.github.com>
2021-12-07 13:00:47 +0100

    adding new route

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Role from '../pages/role';
 import Home from '../pages/Home';
 import Games from '../pages/Games';
+import Tags from '../pages/Tags';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/role" exact component={Role} />
     <Route path="/games" exact component={Games} />
+    <Route path="/tags" exact component={Tags} />
   </Switch>
 );
 export default Routes;
