179f8ebb8e12bd89b13aa8fa797fefca1e4702b7
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2021-12-07 21:35:17 +0100

    removed errors exept 403 and 405

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -1,16 +1,15 @@
 import { useHistory } from 'react-router-dom';
+import { useTranslation } from '../hooks/useTranslation';
+
+const trans = useTranslation();
 
 const handle403 = () => {
   const navigate = useHistory();
   navigate.push('/role');
 };
 
-const handle500 = (message: string) => {
-  alert(message);
-};
-
-const handle404 = (message: string) => {
-  alert(message);
+const handle500 = () => {
+  alert(trans('Error on our side'));
 };
 
 const handleErrors = (error: any) => {
@@ -18,11 +17,8 @@ const handleErrors = (error: any) => {
     case 403:
       handle403();
       break;
-    case 404:
-      handle404(error.response.data.message);
-      break;
     case 500:
-      handle500(error.response.data.message);
+      handle500();
       break;
     default:
   }
