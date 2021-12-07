206a3886b03fe973421261d4cdf48a9f5b611dd6
Were42 <50260518+Were42@users.noreply.github.com>
2021-12-07 13:01:37 +0100

    creating table component

--- src/components/TagsTable.tsx
+++ src/components/TagsTable.tsx
@@ -1,44 +1,86 @@
 import React, { FC } from 'react';
 import { useTable } from 'react-table';
+import { Tag } from '../pages/Tags';
 
-const TagsTable: FC = () => {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World'
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks'
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want'
-      };
-    ]
-    []
-  )
+type Props = {
+  tagsData: Tag[]
+}
+const TagsTable: FC<Props> = ({ tagsData }) => {
+  console.log('ZACATEk');
 
+  const data: any[] = [];
+  tagsData.forEach((element) => data.push({ col1: element.nameCs, col2: element.nameEn }));
+
+  console.log(data);
+  console.log('KONEC');
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Column 1',
-        accessor: 'col1'
-      },
-      {
-        Header: 'Column 2',
-        accessor: 'col2'
+        Header: 'Tags',
+        columns: [
+          {
+            Header: 'Czech name',
+            accessor: 'col1'
+          },
+          {
+            Header: 'English name',
+            accessor: 'col2'
+          }
+        ]
       }
     ],
     []
   );
-
-  const tableInstance = useTable({ columns, data });
-
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({ columns, data });
   return (
-    <>
-
-    </>
+    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{
+                  borderBottom: 'solid 3px red',
+                  background: 'aliceblue',
+                  color: 'black',
+                  fontWeight: 'bold'
+                }}
+              >
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td
+                  {...cell.getCellProps()}
+                  style={{
+                    padding: '10px',
+                    border: 'solid 1px gray',
+                    background: 'papayawhip'
+                  }}
+                >
+                  {cell.render('Cell')}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
+export default TagsTable;
