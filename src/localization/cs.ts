d0ba930b70dab61366916c96173e7e5ea036f8c5
Ondra Holub <ondrejholub202@gmail.com>
2021-12-09 19:33:08 +0100

    Update cs translation

--- src/localization/cs.ts
+++ src/localization/cs.ts
@@ -10,7 +10,7 @@ export default {
   ErrPassword: 'Heslo musí být dlouhé alespoň 6 znaků',
   Log: 'Přihlášení',
   Password: 'Heslo (aspoň 6 znaků)',
-  ForgPassword: 'Zapomněl jste heslo?',
+  ForgPassword: 'Zapomněl/a jsi heslo?',
   Pswdreneval: 'Obnova hesla',
   'We are sorry but your account was banned!': 'Je nám líto, ale byl ti zablokován účet!',
   'We are waiting for admins to verif your account.': 'Čekáme na to, až administrátoři ověří tvůj účet!',
