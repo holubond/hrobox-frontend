e87581a9d3a588bf154aebb72878091467174e29
Ondra Holub <ondrejholub202@gmail.com>
2021-12-09 18:40:50 +0100

    Extract selectValues

---
+++ src/components/SelectValues.tsx
@@ -0,0 +1,61 @@
+import { Button, FormGroup, SelectMenu } from '@primer/components';
+import React, { FC } from 'react';
+import checked from '../assets/checked.svg';
+import unchecked from '../assets/unchecked.svg';
+import useForceUpdate from '../hooks/useForceUpdate';
+
+type Props = {
+  header: string,
+  allowedValues: any[],
+  values: any[],
+  setValues: React.Dispatch<React.SetStateAction<any[]>>
+  // eslint-disable-next-line no-unused-vars
+  mapValues?: (x: any) => any
+};
+
+const SelectValues: FC<Props> = ({
+  header, allowedValues, values, setValues, mapValues = () => {}
+}) => {
+  const forceUpdate = useForceUpdate();
+
+  const toggleValue = (d: any) => {
+    const newDurations = values;
+    const index = newDurations.indexOf(d);
+    if (index === -1) {
+      newDurations.push(d);
+    } else {
+      newDurations.splice(index, 1);
+    }
+    forceUpdate();
+    setValues(newDurations);
+  };
+
+  return (
+    <FormGroup>
+      <SelectMenu>
+        <Button as="summary">{header}</Button>
+        <SelectMenu.Modal>
+          <SelectMenu.Header>{header}</SelectMenu.Header>
+          <SelectMenu.List>
+            {allowedValues.map((dur) => (
+              <SelectMenu.Item onClick={(e) => { e.preventDefault(); toggleValue(dur); }}>
+                { values.includes(dur) ? (
+                  <img src={checked} height="16" alt={dur} />
+                ) : (
+                  <img src={unchecked} height="16" alt={dur} />
+                )}
+                {mapValues(dur)}
+              </SelectMenu.Item>
+            ))}
+          </SelectMenu.List>
+        </SelectMenu.Modal>
+      </SelectMenu>
+    </FormGroup>
+  );
+};
+
+SelectValues.defaultProps = {
+  mapValues: (x) => x
+};
+
+export default SelectValues;
--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -7,13 +7,12 @@ import routeTo from '../utils/routeTo';
 import handleErrors from '../utils/handleErrors';
 import GamesTable from '../components/GamesTable';
 import SubmitButton from '../components/SubmitButton';
-import checked from '../assets/checked.svg';
-import unchecked from '../assets/unchecked.svg';
 import { Tag } from './Tags';
 import { useLanguage, useTranslation } from '../hooks/useTranslation';
+import { allowedDurations, Duration } from '../model/Duration';
+import SelectValues from '../components/SelectValues';
 
 export type AgeGroup = 'K' | 'S' | 'T' | 'A';
-export type Duration = '<15' | '15-30' | '30-60' | '60+';
 export type Game = {
   id: number,
   version: number,
@@ -39,8 +38,7 @@ const Games = () => {
   const [name, setName] = useState('');
   const [players, setPlayers] = useState<number>();
   const [author, setAuthor] = useState('');
-  const durationAll = ['<15', '15-30', '30-60', '60+'];
-  const [selectedDuration, setSelectedDuration] = useState<Duration[]>([]);
+  const [selectedDurations, setSelectedDurations] = useState<Duration[]>([]);
   const ageGroupsAll = ['K', 'S', 'T', 'A'];
   const [selectedAge, setSelectedAge] = useState<AgeGroup[]>([]);
   const [tagFilter, setTagFilter] = useState('');
@@ -48,24 +46,6 @@ const Games = () => {
   const [allRemainingTags, setAllRemainingTags] = useState<Tag[]>([]);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
-  const clickDur = (duration: string) => {
-    let newDur = selectedDuration;
-    if (newDur?.find((element) => element === (duration as Duration)) === undefined) {
-      newDur?.push(duration as Duration);
-    } else {
-      newDur = newDur.filter((element) => element !== duration);
-    }
-    setSelectedDuration(newDur);
-  };
-  const clickAge = (age: string) => {
-    let newAge = selectedAge;
-    if (newAge?.find((element) => element === (age as AgeGroup)) === undefined) {
-      newAge?.push(age as AgeGroup);
-    } else {
-      newAge = newAge.filter((element) => element !== age);
-    }
-    setSelectedAge(newAge);
-  };
   const clickTag = (tag: Tag) => {
     let newTag = selectedTags;
     if (newTag.find((element) => element === tag) === undefined) {
@@ -75,6 +55,7 @@ const Games = () => {
     }
     setSelectedTags(newTag);
   };
+
   const tagFilterChange = (e: any) => {
     let newFilteredTags: Tag[];
     if ((e.target.value as string).length > tagFilter.length) {
@@ -109,7 +90,7 @@ const Games = () => {
       ...author.length > 0 ? { author } : {},
       ...name.length > 0 ? { name } : {},
       ...selectedAge.length > 0 ? { ageGroup } : {},
-      ...selectedDuration.length > 0 ? { duration: selectedDuration } : {},
+      ...selectedDurations.length > 0 ? { duration: selectedDurations } : {},
       ...selectedTags.length > 0 ? { tags } : {},
       ...players !== undefined ? { players } : {}
     };
@@ -119,7 +100,6 @@ const Games = () => {
   const getFilteredGames = (e: any) => {
     e.preventDefault();
     setLoading(true);
-    // const filter = setupFilterPost();
     const filter = setupFilterPost();
     axios.post(routeTo('/api/games'), filter)
       .then((response) => {
@@ -161,11 +141,12 @@ const Games = () => {
     getAllGames();
   }, [selectedLang]);
   useEffect(() => {
-  }, [selectedDuration]);
+  }, [setSelectedDurations]);
   return (
     <>
       <Box sx={{ width: '90%', display: 'flex', flexDirection: 'row' }}>
         <form onSubmit={getFilteredGames}>
+
           {/* Name contain */}
           <FormGroup>
             <TextInput
@@ -174,52 +155,24 @@ const Games = () => {
               onChange={(e: any) => setName(e.target.value)}
             />
           </FormGroup>
+
           {/* duration */}
-          <FormGroup>
-            <SelectMenu>
-              <Button as="summary">{trans('Duration')}</Button>
-              <SelectMenu.Modal>
-                <SelectMenu.Header>{trans('Duration')}</SelectMenu.Header>
-                <SelectMenu.List>
-                  {durationAll.map((dur) => (
-                    <SelectMenu.Item onClick={() => clickDur(dur)}>
-                      {selectedDuration?.find(
-                        (element) => element === (dur as Duration)
-                      ) === undefined ? (
-                        <img src={unchecked} height="16" alt={dur} />
-                        ) : (
-                          <img src={checked} height="16" alt={dur} />
-                        )}
-                      {dur}
-                    </SelectMenu.Item>
-                  ))}
-                </SelectMenu.List>
-              </SelectMenu.Modal>
-            </SelectMenu>
-          </FormGroup>
+          <SelectValues
+            header={trans('Duration')}
+            allowedValues={allowedDurations}
+            values={selectedDurations}
+            setValues={setSelectedDurations}
+          />
+
           {/* Age */}
-          <FormGroup>
-            <SelectMenu>
-              <Button as="summary">{trans('Age groups')}</Button>
-              <SelectMenu.Modal>
-                <SelectMenu.Header>{trans('Age groups')}</SelectMenu.Header>
-                <SelectMenu.List>
-                  {ageGroupsAll.map((age) => (
-                    <SelectMenu.Item onClick={() => clickAge(age)}>
-                      {selectedAge?.find(
-                        (element) => element === (age as AgeGroup)
-                      ) === undefined ? (
-                        <img src={unchecked} height="16" alt={trans(age as AgeGroup)} />
-                        ) : (
-                          <img src={checked} height="16" alt={trans(age as AgeGroup)} />
-                        )}
-                      {trans(age as AgeGroup)}
-                    </SelectMenu.Item>
-                  ))}
-                </SelectMenu.List>
-              </SelectMenu.Modal>
-            </SelectMenu>
-          </FormGroup>
+          <SelectValues
+            header={trans('Age groups')}
+            allowedValues={ageGroupsAll}
+            values={selectedAge}
+            setValues={setSelectedAge}
+            mapValues={trans}
+          />
+
           {/* number of players */}
           <FormGroup>
             <TextInput
@@ -228,6 +181,7 @@ const Games = () => {
               onChange={(e: any) => setPlayers(e.target.value)}
             />
           </FormGroup>
+
           {/* Tags */}
           <FormGroup>
             <SelectMenu>
@@ -246,6 +200,7 @@ const Games = () => {
             </SelectMenu>
             {selectedTags.map((tag) => <Label>{selectedLang === 'cs' ? tag.nameCs : tag.nameEn}</Label>)}
           </FormGroup>
+
           {/* Author contain */}
           <FormGroup>
             <TextInput
